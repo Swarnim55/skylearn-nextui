c090cc1090c74455def6bad0c8c0e91d8eca2b75
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-12-20 22:52:18 +0545

    fix: fetch

--- app/dashboard/staff/create/page.tsx
+++ app/dashboard/staff/create/page.tsx
@@ -1,24 +1,24 @@
-"use client";
-import React, { useEffect, useMemo, useState } from "react";
-import { ReactDOM } from "react";
-import { z } from "zod";
-import { PORTAL_BASE_URL, getApiRoute } from "../../../../constants";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { Input } from "@nextui-org/input";
+'use client';
+import React, { useEffect, useMemo, useState } from 'react';
+import { ReactDOM } from 'react';
+import { z } from 'zod';
+import { PORTAL_BASE_URL, getApiRoute } from '../../../../constants';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Input } from '@nextui-org/input';
 import {
   Autocomplete,
   AutocompleteItem,
   Button,
   Select,
   SelectItem,
-} from "@nextui-org/react";
-import axios from "axios";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { apiRoutes } from "@/constants/apiRoutes";
-import { pageRoutes } from "@/constants/pageRoutes";
-import { map } from "lodash";
-import { selectFieldDataFetcher } from "@/utils/fetch";
+} from '@nextui-org/react';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { apiRoutes } from '@/constants/apiRoutes';
+import { pageRoutes } from '@/constants/pageRoutes';
+import { map } from 'lodash';
+import { selectFieldDataFetcher } from '@/utils/fetch';
 
 interface StaffFormDataType {
   firstName: string;
@@ -35,15 +35,15 @@ interface StaffFormDataType {
 }
 
 const genders = [
-  { label: "Male", value: "male" },
-  { label: "Female", value: "female" },
-  { label: "Other", value: "other" },
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'Other', value: 'other' },
 ];
 
 const departments = [
-  { Pid: "Technical departmentzzz", name: "dep_b55c69a5a510" },
-  { Pid: "Civil Engineering", name: "dep_1713679a2255" },
-  { Pid: "ECE", name: "dep_42f6f517427e" },
+  { Pid: 'Technical departmentzzz', name: 'dep_b55c69a5a510' },
+  { Pid: 'Civil Engineering', name: 'dep_1713679a2255' },
+  { Pid: 'ECE', name: 'dep_42f6f517427e' },
 ];
 
 const CreatePage = () => {
@@ -53,24 +53,29 @@ const CreatePage = () => {
     formState: { errors },
   } = useForm();
   const { data: sessionData } = useSession();
+  const [deptOpt, setDeptOpt] = useState<string[]>([]);
   const { jwtToken } = sessionData?.user?.data || {};
   const router = useRouter();
-  const [dept_opt, set_dept_opt] = useState([]);
-  const department_options = useMemo(() => {
-    const resdata = selectFieldDataFetcher(
-      getApiRoute("DEPARTMENTS"),
-      "departmentName",
-      jwtToken
-    );
-    set_dept_opt(resdata);
-  }, [dept_opt]);
 
-  console.log(department_options);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resdata = await selectFieldDataFetcher(
+        getApiRoute('DEPARTMENTS'),
+        'departmentName',
+        jwtToken
+      );
+      setDeptOpt(resdata);
+    };
+
+    fetchData();
+  }, [jwtToken]);
+
+  console.log('deptOpt', deptOpt);
   const onSubmit: SubmitHandler<StaffFormDataType> = async (data) => {
     try {
-      var path = getApiRoute("STAFF");
+      var path = getApiRoute('STAFF');
       const response = await axios({
-        method: "POST",
+        method: 'POST',
         url: `${PORTAL_BASE_URL}${path}`,
         data: data,
         headers: {
@@ -81,7 +86,7 @@ const CreatePage = () => {
         router.push(pageRoutes.STAFF);
       }
     } catch (error) {
-      console.error("API Error:", error);
+      console.error('API Error:', error);
     }
   };
   return (
@@ -89,20 +94,20 @@ const CreatePage = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex w-full flex-wrap md:flex-nowrap gap-4 py-4">
           <Input
-            {...register("firstName")}
+            {...register('firstName')}
             isRequired
             type="text"
             label="First Name"
             placeholder="Enter your first name"
           />
           <Input
-            {...register("middleName")}
+            {...register('middleName')}
             type="text"
             label="Middle Name"
             placeholder="Enter your middle name"
           />
           <Input
-            {...register("lastName")}
+            {...register('lastName')}
             isRequired
             type="text"
             label="Last Name"
@@ -111,21 +116,21 @@ const CreatePage = () => {
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap gap-4 py-4">
           <Input
-            {...register("email")}
+            {...register('email')}
             isRequired
             type="email"
             label="Email"
             placeholder="Enter your email"
           />
           <Input
-            {...register("address")}
+            {...register('address')}
             isRequired
             type="address"
             label="Address"
             placeholder="Enter your address"
           />
           <Input
-            {...register("phone")}
+            {...register('phone')}
             isRequired
             type="text"
             label="phone"
@@ -134,7 +139,7 @@ const CreatePage = () => {
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap gap-4 py-4">
           <Select
-            {...register("gender")}
+            {...register('gender')}
             label="Select your gender"
             className="max-w-xs"
           >
@@ -145,14 +150,14 @@ const CreatePage = () => {
             ))}
           </Select>
           <Input
-            {...register("dob")}
+            {...register('dob')}
             isRequired
             type="date"
             label="Birth Date"
             placeholder="Enter date of birth"
           />
           <Input
-            {...register("joiningDate")}
+            {...register('joiningDate')}
             isRequired
             type="date"
             label="Join Date"
@@ -161,19 +166,18 @@ const CreatePage = () => {
         </div>
         <div className="flex w-1/2 flex-wrap md:flex-nowrap gap-4 py-4">
           <Select
-            {...register("departmentId")}
+            {...register('departmentId')}
             label="Select Department"
             className="max-w-xs"
           >
-            {Array.isArray(department_options) &&
-              department_options.map((dep) => (
-                <SelectItem key={dep.value} value={dep.value}>
-                  {dep.label}
-                </SelectItem>
-              ))}
+            {deptOpt.map((dep) => (
+              <SelectItem key={dep.value} value={dep.value}>
+                {dep.label}
+              </SelectItem>
+            ))}
           </Select>
           <Input
-            {...register("postion")}
+            {...register('postion')}
             isRequired
             type="text"
             label="Position"
